533f289620b74cf6950fa94537fa02a616c3bbfe
Théo WEIMANN <theo.weimann@viacesi.fr>
2021-11-22 23:50:00 -0500

    Adding file stub

---
+++ src/PacketLocator.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+print('[SRC] Hello world!')
+
---
+++ test/TestPacketLocator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class TestPacketLocator < Minitest::Test
+  def test_1
+    lambda do
+      assert_equal false, true
+    end
+  end
+end
