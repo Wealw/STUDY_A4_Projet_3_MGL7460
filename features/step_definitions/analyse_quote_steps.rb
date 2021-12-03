cc731a5199f46fb3adffd989426afaea24a4febe
Théo Weimann <theo.weimann.priv@gmail.com>
2021-12-02 21:55:34 -0800

    Replace citation to quote since wasn't proper english

---
+++ features/step_definitions/analyse_quote_steps.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Given(/^the database contains the following quote : (.*)$/) do |_input|
+  pending
+end
+
+When(/^the analyse is run$/) do
+  pending
+end
+
+Then(/^the printed output should be (.*)$/) do |_output|
+  pending
+end
