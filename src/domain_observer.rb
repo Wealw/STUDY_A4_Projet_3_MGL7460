859d23e27aa9e9d87f89028c20356ed8c5e407c8
Théo Weimann <theo.weimann.priv@gmail.com>
2021-12-04 20:46:21 -0800

    Add the domain_observer main script. It handle the flags and associated parameters

---
+++ src/domain_observer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require './src/domain_helper'
+
+OptionParser.new do |parser|
+  parser.on('-a', '--add=DOMAIN') do |domain|
+    DomainHelper.create_domain domain
+  end
+  parser.on('-d', '--delete=DOMAIN') do |domain|
+    DomainHelper.delete_domain domain
+  end
+  parser.on('-s', '--show') do
+    DomainHelper.analyse_domains
+  end
+end.parse!
+exit(0)
