afda4a2f9912587729c65d4790c9ddbc7cf8ba72
Théo Weimann <theo.weimann.priv@gmail.com>
2021-12-04 19:45:21 -0800

    Fill the test for the domain helper

--- test/domain_helper_test.rb
+++ test/domain_helper_test.rb
@@ -3,27 +3,62 @@
 require 'minitest/autorun'
 require 'minitest'
 require './src/domain_helper'
+require 'database_cleaner'
 
 class DomainHelperTest < Minitest::Test
+  DatabaseCleaner.strategy = :truncation
 
   def test_create_domain
-    #DomainHelper.create_domain "example.com"
-    assert_equal true, false
+    # Setup
+    DatabaseCleaner.clean
+    # Test
+    capture_io do
+      DomainHelper.create_domain 'example.com'
+    end
+    # Assertion
+    result = Domain.find_by(fqdn: 'example.com')
+    assert !result.nil?
   end
 
   def test_delete_domain
-    #DomainHelper.delete_domain "example.com"
-    assert_equal true, false
+    # Setup
+    DatabaseCleaner.clean
+    Domain.create(fqdn: 'example.com')
+    # Test
+    capture_io do
+      DomainHelper.delete_domain 'example.com'
+    end
+    # Assertion
+    result = Domain.find_by(fqdn: 'example.com')
+    assert result.nil?
   end
 
   def test_analyse_domains
-    #DomainHelper.create_domain "example1.com"
-    #DomainHelper.create_domain "example2.com"
-    assert_equal true, false
+    # Setup
+    DatabaseCleaner.clean
+    Domain.create(fqdn: 'example1.com')
+    Domain.create(fqdn: 'example2.com')
+    # Test
+    result, = capture_io do
+      DomainHelper.analyse_domains
+    end
+    # Assertion
+    assert !result.match(/is owned with the following NS record|is currently not used by anybody/).nil?
   end
 
   def test_analyse_from_json
-    #DomainHelper.analyse_from_json "example.com", "{}"
-    assert_equal true, false
+    # Setup
+    DatabaseCleaner.clean
+    Domain.create(fqdn: 'example.com')
+    domain = Domain.find_by(fqdn: 'example.com')
+    json = JSON.parse '{"status": "success", "message": "NS record has been retrieved for example.com successful'\
+                              'ly", "data": {"query": {"type": "NS", "name": "example.com"}, "answer": ["b.iana-server'\
+                              's.net.", "a.iana-servers.net."]}}'
+    # Test
+    result, = capture_io do
+      DomainHelper.analyse_from_json domain, json
+    end
+    # Assertion
+    assert !result.match(/is owned with the following NS record|is currently not used by anybody/).nil?
   end
 end
